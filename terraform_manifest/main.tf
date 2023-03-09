013fc272df350478b11ff45d6d598448eb0f9584
Philip <phdah.sjoberg@gmail.com>
2023-03-09 17:22:18 +0100

    Setup manifest possible terraform deploy of Argo CD

---
+++ terraform_manifest/main.tf
@@ -0,0 +1,82 @@
+terraform {
+    required_providers {
+        kind = {
+            source = "tehcyx/kind"
+                version = "0.0.16"
+        }
+        kubectl = {
+            source  = "gavinbunney/kubectl"
+                version = "1.14.0"
+        }
+    }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "argo_test" {
+    name = "argo-test"
+        wait_for_ready = true
+        node_image = "kindest/node:v1.26.0"
+        kind_config {
+            kind = "Cluster"
+                api_version = "kind.x-k8s.io/v1alpha4"
+
+            node {
+                role = "control-plane"
+            }
+
+            node {
+                role = "worker"
+            }
+
+            node {
+                role = "worker"
+            }
+
+            node {
+                role = "worker"
+            }
+        }
+}
+
+provider "kubectl" {
+        host                    = "${kind_cluster.argo_test.endpoint}"
+        cluster_ca_certificate  = "${kind_cluster.argo_test.cluster_ca_certificate}"
+        client_certificate      = "${kind_cluster.argo_test.client_certificate}"
+        client_key              = "${kind_cluster.argo_test.client_key}"
+        load_config_file        = false
+}
+
+resource "kubectl_manifest" "namespace" {
+    depends_on = [
+      kind_cluster.argo_test,
+    ]
+    yaml_body = file("../app/namespace.yml")
+}
+
+data "kubectl_file_documents" "argocd" {
+    content = file("../app/argocd.yml")
+}
+
+resource "kubectl_manifest" "argocd" {
+    depends_on = [
+      kubectl_manifest.namespace,
+    ]
+    count = length(data.kubectl_file_documents.argocd.documents)
+    yaml_body = element(data.kubectl_file_documents.argocd.documents, count.index)
+    override_namespace = "argocd"
+}
+
+
+data "kubectl_file_documents" "application" {
+    content = file("../app/application.yml")
+}
+
+resource "kubectl_manifest" "application" {
+    depends_on = [
+      kubectl_manifest.argocd,
+    ]
+    count = length(data.kubectl_file_documents.application.documents)
+    yaml_body = element(data.kubectl_file_documents.application.documents, count.index)
+    override_namespace = "argocd"
+}
